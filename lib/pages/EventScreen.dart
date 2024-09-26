90364213457566e67ea983015998e6c8b016b3c4
Touka-Agra <touka.agra@gmail.com>
2024-09-26 23:40:46 +0300

    touka check

---
+++ lib/pages/EventScreen.dart
@@ -0,0 +1,13 @@
+class EventScreen extends StatefulWidget {
+  const EventScreen({super.key});
+
+  @override
+  State<EventScreen> createState() => _EventScreenState();
+}
+
+class _EventScreenState extends State<EventScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
