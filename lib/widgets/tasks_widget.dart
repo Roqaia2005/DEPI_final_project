92447239d20e79dc4f12019cee2bcaadaa52aa45
Roqaia Hassan <roqaiahassanahmed@gmail.com>
2024-09-27 00:07:51 +0300

    test editing

--- lib/widgets/tasks_widget.dart
+++ lib/widgets/tasks_widget.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
@@ -72,3 +70,4 @@ class _TasksWidgetState extends State<TasksWidget> {
     );
   }
 }
+//test editing 
